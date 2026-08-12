824935bcb42a6bbaffd32879d1826389a75b06c5
pututpap01 <152639678+pututpap01@users.noreply.github.com>
2026-08-12 13:43:17 +0700

    Fix GitHub Actions build script and keystore fallback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,11 +31,14 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
+    val customKeystore = file("${rootDir}/debug.keystore")
+    if (customKeystore.exists()) {
+      create("debugConfig") {
+        storeFile = customKeystore
+        storePassword = "android"
+        keyAlias = "androiddebugkey"
+        keyPassword = "android"
+      }
     }
   }
 
@@ -46,7 +49,13 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
+    debug {
+      signingConfig = if (signingConfigs.findByName("debugConfig") != null) {
+        signingConfigs.getByName("debugConfig")
+      } else {
+        signingConfigs.getByName("debug")
+      }
+    }
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
